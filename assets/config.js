a11a709e1ae7abef78639d21ed38b76118f0b9ae
MD. ABDULLAH AL FUAD <fuadkalaroa2002@gmail.com>
2026-08-26 19:59:18 +0600

    Update production bundle and security configurations

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 window.LGS_CONFIG = {
-  API_URL: "https://fighter-purchased-reel-federal.trycloudflare.com",
+  API_URL: "https://welfare-luis-kijiji-coupons.trycloudflare.com",
   SCRAPER_URL: "http://localhost:4000"
 };
