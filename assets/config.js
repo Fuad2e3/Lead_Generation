4fd5add44168e04997e7fb5ed0ca031e77a4770c
MD. ABDULLAH AL FUAD <fuadkalaroa2002@gmail.com>
2026-08-26 18:24:20 +0600

    Sync active Cloudflare tunnel in config.js

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 window.LGS_CONFIG = {
-  API_URL: "https://safer-membership-workflow-yacht.trycloudflare.com",
+  API_URL: "https://social-click-gene-purchases.trycloudflare.com",
   SCRAPER_URL: "http://localhost:4000"
 };
