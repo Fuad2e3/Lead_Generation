7cd95bbefbc16902c3e249279789bf4e3e6a9319
MD. ABDULLAH AL FUAD <fuadkalaroa2002@gmail.com>
2026-08-26 18:35:33 +0600

    Update active Cloudflare tunnel in config.js

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 window.LGS_CONFIG = {
-  API_URL: "https://weights-survival-prince-eco.trycloudflare.com",
+  API_URL: "https://boutique-flashers-year-research.trycloudflare.com",
   SCRAPER_URL: "http://localhost:4000"
 };
