85668a708d42c83388636574c0049055ac2e56c4
MD. ABDULLAH AL FUAD <fuadkalaroa2002@gmail.com>
2026-08-26 14:06:05 +0600

    Update Cloudflare Tunnel URL and move start-servers to dev

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 window.LGS_CONFIG = {
-  API_URL: "https://toolbar-frog-genres-giants.trycloudflare.com",
+  API_URL: "https://immunology-dance-actively-join.trycloudflare.com",
   SCRAPER_URL: "http://localhost:4000"
 };
