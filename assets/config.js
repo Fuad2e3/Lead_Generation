61a09afb51336fb12bc6e61728bde23a380a5cac
MD. ABDULLAH AL FUAD <fuadkalaroa2002@gmail.com>
2026-08-26 19:04:32 +0600

    Update active Cloudflare tunnel in config.js

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 window.LGS_CONFIG = {
-  API_URL: "https://promising-lime-scientists-page.trycloudflare.com",
+  API_URL: "https://fighter-purchased-reel-federal.trycloudflare.com",
   SCRAPER_URL: "http://localhost:4000"
 };
