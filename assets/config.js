4e4c748fc6823159484e875642f3d6b4c5285a39
MD. ABDULLAH AL FUAD <fuadkalaroa2002@gmail.com>
2026-08-26 18:17:08 +0600

    .

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 window.LGS_CONFIG = {
-  API_URL: "https://carefully-era-characterization-weblogs.trycloudflare.com",
+  API_URL: "https://safer-membership-workflow-yacht.trycloudflare.com",
   SCRAPER_URL: "http://localhost:4000"
 };
