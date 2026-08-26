b1cc6d1eb48e5c0a5d631b878f8432329c424a87
MD. ABDULLAH AL FUAD <fuadkalaroa2002@gmail.com>
2026-08-26 18:29:58 +0600

    Update active Cloudflare tunnel in config.js

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 window.LGS_CONFIG = {
-  API_URL: "https://lamb-eminem-empty-gordon.trycloudflare.com",
+  API_URL: "https://heard-certification-advertisers-cheque.trycloudflare.com",
   SCRAPER_URL: "http://localhost:4000"
 };
