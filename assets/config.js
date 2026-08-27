39b7f744857ddff2c83dcb3f23f31a388882e582
MD. ABDULLAH AL FUAD <fuadkalaroa2002@gmail.com>
2026-08-27 15:24:27 +0600

    .

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 window.LGS_CONFIG = {
-  API_URL: "https://pools-crucial-institution-lists.trycloudflare.com",
+  API_URL: "https://highways-hebrew-footwear-max.trycloudflare.com",
   SCRAPER_URL: "http://localhost:4000"
 };
