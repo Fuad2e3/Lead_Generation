b4ea3a31bcc008770ef0012096bd6d3575526f22
MD. ABDULLAH AL FUAD <fuadkalaroa2002@gmail.com>
2026-08-27 13:19:12 +0600

    .

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 window.LGS_CONFIG = {
-  API_URL: "https://welfare-luis-kijiji-coupons.trycloudflare.com",
+  API_URL: "https://pools-crucial-institution-lists.trycloudflare.com",
   SCRAPER_URL: "http://localhost:4000"
 };
