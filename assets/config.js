03bf6dfb43250a35b8d0d08098d44fdf7f482456
MD. ABDULLAH AL FUAD <fuadkalaroa2002@gmail.com>
2026-08-28 19:25:59 +0600

    .

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 window.LGS_CONFIG = {
-  API_URL: "https://maui-labeled-pre-msgstr.trycloudflare.com",
+  API_URL: "https://instrument-subjects-difficulty-velocity.trycloudflare.com",
   SCRAPER_URL: "http://localhost:4000"
 };
