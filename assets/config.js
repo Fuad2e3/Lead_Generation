6c5d2ca916a4fa89f40ef902a2cbd02bb95d7d75
MD. ABDULLAH AL FUAD <fuadkalaroa2002@gmail.com>
2026-08-31 14:03:08 +0600

    Update API gateway endpoint in config

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 window.LGS_CONFIG = {
-  API_URL: "https://instrument-subjects-difficulty-velocity.trycloudflare.com",
+  API_URL: "https://knowledgestorm-features-measures-dozens.trycloudflare.com",
   SCRAPER_URL: "http://localhost:4000"
 };
