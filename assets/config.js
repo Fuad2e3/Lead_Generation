a91d99e66a8889762dd276f27835934384b67bc8
MD. ABDULLAH AL FUAD <fuadkalaroa2002@gmail.com>
2026-09-02 09:43:30 +0600

    .

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 window.LGS_CONFIG = {
-  API_URL: "https://verification-clerk-scholarships-towers.trycloudflare.com",
+  API_URL: "https://geneva-genius-ira-affiliates.trycloudflare.com",
   SCRAPER_URL: "http://localhost:4000"
 };
