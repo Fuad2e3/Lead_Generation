cb70d5fee6fa0e1e9c483a646541dd0b26c6460b
MD. ABDULLAH AL FUAD <fuadkalaroa2002@gmail.com>
2026-09-03 17:54:50 +0600

    .

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 window.LGS_CONFIG = {
-  API_URL: "https://geneva-genius-ira-affiliates.trycloudflare.com",
+  API_URL: "https://success-wins-floyd-employees.trycloudflare.com",
   SCRAPER_URL: "http://localhost:4000"
 };
