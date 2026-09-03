80ddc4c806012e3e775b9d4ea7b839385a9e2630
MD. ABDULLAH AL FUAD <fuadkalaroa2002@gmail.com>
2026-09-03 21:01:39 +0600

    .

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 window.LGS_CONFIG = {
-  API_URL: "https://followed-nicole-focuses-paths.trycloudflare.com",
+  API_URL: "https://wishing-pushed-televisions-blanket.trycloudflare.com",
   SCRAPER_URL: "http://localhost:4000"
 };
